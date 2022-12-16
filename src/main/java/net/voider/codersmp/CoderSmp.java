723cec42372943e73a478b643eb4c70b53bb5c2e
voider32 <rowankilroy@gmail.com>
2022-12-15 21:29:25 -0600

    0.0.1

---
+++ src/main/java/net/voider/codersmp/CoderSmp.java
@@ -0,0 +1,16 @@
+package net.voider.codersmp;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+// Very important comment
+public class CoderSmp implements ModInitializer {
+	public static final String MOD_ID = "codersmp";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/voider/codersmp/CoderSmpClient.java
@@ -0,0 +1,10 @@
+package net.voider.codersmp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoderSmpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
